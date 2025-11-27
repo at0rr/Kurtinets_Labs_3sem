570bcb2252bc72db690d38fa68cff1e48cc8f9d2
at0rr <romakurtinets07@gmail.com>
2025-11-27 10:33:28 +0300

    add lab_5

---
+++ MyClassLibrary/LevenshteinDistance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyClassLibrary;
+
+public static class LevenshteinDistance
+{
+    public static int Calculate(string ftWord, string secWord)
+    {
+        int ftLen = ftWord.Length;
+        int secLen = secWord.Length;
+
+        var matrix = new int[ftLen + 1, secLen + 1];
+
+        for (int i = 0; i <= ftLen; ++i) matrix[i, 0] = i;
+
+        for (int i = 0; i <= secLen; ++i) matrix[0, i] = i;
+
+        for (int i = 1; i <= ftLen; ++i)
+        {
+            for (int j = 1; j <= secLen; ++j)
+            {
+                int cost = ftWord[i - 1] == secWord[j - 1] ? 0 : 1;
+
+                int deletion = matrix[i - 1, j] + 1;
+                int insertion = matrix[i, j - 1] + 1;
+                int substitution = matrix[i - 1, j - 1] + cost;
+
+                matrix[i, j] = Math.Min(Math.Min(deletion, insertion), substitution);
+            }
+        }
+
+        return matrix[ftLen, secLen];
+    }
+
+    public static void Main()
+    {
+        System.Console.WriteLine(Calculate("слово", "строка"));
+        System.Console.WriteLine(Calculate("кот", "кит"));
+        System.Console.WriteLine(Calculate("", "привет"));
+        System.Console.WriteLine(Calculate("hello", "hello"));
+    }
+}
